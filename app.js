e3e0ecbe1ae19a8c367fd5a9776a11e75b0c45f4
CqVcoraet <149435569+CqVcoraet@users.noreply.github.com>
2024-04-30 11:14:32 -0600

    Added the background.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// Background
+var bg = document.createElement("div");
+bg.style.width = "1000px";
+bg.style.height = "1600px";
+bg.style.left = "50%";
+bg.style.top = "50%";
+bg.style.transform = "translate(-50%, -50%)";
+bg.style.backgroundColor = "#03468F";
+document.body.append(bg);
+
 
